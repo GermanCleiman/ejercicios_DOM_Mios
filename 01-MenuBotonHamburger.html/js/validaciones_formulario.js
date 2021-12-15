fb5577f0d0c1c303c7a14e0e3b19adbdc46d1738
GermanCleiman <cleimangerman@gmail.com>
2021-12-15 18:44:22 -0300

    15/12-2

--- 01-MenuBotonHamburger.html/js/validaciones_formulario.js
+++ 01-MenuBotonHamburger.html/js/validaciones_formulario.js
@@ -10,6 +10,15 @@ $inputs.forEach((input)=>{
   const $span =d.createElement("span");
   $span.id = input.name;//el name del input
   $span.textContent = input.title;//el title del input
-  input.insertAdjacentElement("afterend", $span)/*pongo el $spam debajo del input */
-})
+  $span.classList.add("contact-form-error","none");
+  input.insertAdjacentElement("afterend", $span);/*pongo el $spam debajo del input */
+});
+
+d.addEventListener("keyup", (e)=>{
+  if(e.target.matches(".contact-form [required]")){
+    let $input = e.target,
+    pattern = $input.pattern || $input.dataset.pattern;//si tiene pattern ó si tiene data-pattern
+    console.log($input, pattern);
+  }
+});
 }
