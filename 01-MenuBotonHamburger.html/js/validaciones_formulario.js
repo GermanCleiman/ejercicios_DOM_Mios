3e2ae8bea85333ba8bc88fbdb8cbed9e15ae94b0
GermanCleiman <cleimangerman@gmail.com>
2021-12-15 18:44:47 -0300

    15/12-3

--- 01-MenuBotonHamburger.html/js/validaciones_formulario.js
+++ 01-MenuBotonHamburger.html/js/validaciones_formulario.js
@@ -4,13 +4,13 @@ export default function contactFormValidations(){
 const $form = d.querySelector(".contact-form"),
 $inputs = d.querySelectorAll(".contact-form [required]");
 
-console.log($inputs);
+//console.log($inputs);
 
 $inputs.forEach((input)=>{
   const $span =d.createElement("span");
   $span.id = input.name;//el name del input
   $span.textContent = input.title;//el title del input
-  $span.classList.add("contact-form-error","none");
+  $span.classList.add("contact-form-error", "none");
   input.insertAdjacentElement("afterend", $span);/*pongo el $spam debajo del input */
 });
 
@@ -18,7 +18,23 @@ d.addEventListener("keyup", (e)=>{
   if(e.target.matches(".contact-form [required]")){
     let $input = e.target,
     pattern = $input.pattern || $input.dataset.pattern;//si tiene pattern ó si tiene data-pattern
-    console.log($input, pattern);
+    //console.log($input, pattern);
+    if(pattern && $input.value !== ""){
+      //console.log("el input tiene patrón");
+      let regex = new RegExp(pattern);//con el pattern creo una nueva exprecion regular
+      // si $regex no cumple con la exprecion regular
+      //activa el ID
+      //desactiva el ID
+      return !(regex).exec($input.value)
+      ? d.getElementById($input.name).classList.add("is-active")
+      : d.getElementById($input.name).classList.remove("is-active");
+    }
+    if(!pattern){
+      //console.log("el input No tiene patron");
+      return $input.value === ""
+      ? d.getElementById($input.name).classList.add("is-active")
+      : d.getElementById($input.name).classList.remove("is-active");
+    }
   }
 });
 }
